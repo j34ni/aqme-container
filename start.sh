c355a4c3f850b9f6aaf1f2d3ec3741ae615ca2a3
j34ni <jeani@uio.no>
2025-10-31 12:30:53 +0000

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-. /opt/conda/etc/profile.d/conda.sh
 
-export PATH="/opt/conda/envs/aqme/bin:$PATH"
-export LD_LIBRARY_PATH="/opt/conda/envs/aqme/lib:$LD_LIBRARY_PATH"
-export CONDA_PREFIX="/opt/conda/envs/aqme"
-export PYTHONPATH="/opt/conda/envs/aqme/lib/python3.10/site-packages:$PYTHONPATH"
+. /opt/conda/etc/profile.d/conda.sh && conda activate aqme
 
-if [ $# -eq 0 ]; then
-    exec conda run -n aqme bash
-fi
+export PATH="$CONDA_PREFIX/bin:$PATH"
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="$CONDA_PREFIX/lib/python3.10/site-packages:$PYTHONPATH"
 
-exec conda run -n aqme --no-capture-output "$@"
+exec "$@"
